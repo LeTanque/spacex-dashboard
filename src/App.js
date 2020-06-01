3d477ef96615a5b0c30476ad79eb307b0a03f673
Frank Martinez <frankie.martinez@gmail.com>
2020-05-31 19:39:31 -0700

    :apple: Playing with Antd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from 'antd';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout>
+        <Layout.Header>
+          Header
+        </Layout.Header>
+        <Layout.Content>
+          Your can check your current rate limit and usage details by inspecting the X-RateLimit-Limit and X-RateLimit-Remaining HTTP headers that are returned on every API response. For example, if an API has the default hourly limit of 1,000 request, after making 2 requests, you will receive this HTTP header in the response of the second request:
+        </Layout.Content>
+        <Layout.Footer>
+          Footer
+        </Layout.Footer>
+      </Layout>
+    </>
   );
 }
 
